e3ea399cc76ed94efaf12a466e0e310073b48e2c
Bahrom <bahram101@mail.ru>
2024-11-25 16:56:21 +0500

    cheking query with prisma

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,15 @@
+import { db } from '@/shared/lib/db'
 import { Button } from '@/shared/ui/button'
 import React from 'react'
 
-const Page = () => {
+const Home = async () => {
+  
+  const res = await db.post.findMany()
+  console.log('res',res)
+
   return (
     <div>Home <Button variant={'default'} size="lg" >Button</Button></div>
   )
 }
 
-export default Page
+export default Home
