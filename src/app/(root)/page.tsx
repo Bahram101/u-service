ddb41a9120265fad90296374727448fc767c0163
Bahrom <bahram101@mail.ru>
2024-12-09 13:49:25 +0500

    add swiper

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 
-const Home = async () => {
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const Home = () => {
   return (
-    <div>asdf</div>
+    <div className="h-[600px]">
+      <Swiper
+        pagination={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper h-full"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
--- src/shared/components/layouts/main/header.tsx
+++ src/shared/components/layouts/main/header.tsx
@@ -9,54 +9,57 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 const Header = () => {
   return (
-    <header>
-      <div className="bg-[#F0F2F3]">
-        <Container>
-          <div className="header_top flex justify-between h-[64px] items-center">
-            <div className="logo">Logo</div>
-            <div className="search-bar flex items-center relative">
-              <Input className="bg-[#] w-[400px] border-0 shadow-none" />
-              <CiSearch className="absolute right-2" size={20} />
+   
+      <header>
+        <div className="bg-[#F0F2F3]">
+          <Container>
+            <div className="header_top flex justify-between h-[64px] items-center">
+              <div className="logo">Logo</div>
+              <div className="search-bar flex items-center relative">
+                <Input className="bg-[#] w-[400px] border-0 shadow-none" />
+                <CiSearch className="absolute right-2" size={20} />
+              </div>
+              <div className="flex items-center">
+                <div className="headerButtons">
+                  <PiShoppingCartSimple />
+                  <div className="text-xs pl-1">Cart</div>
+                </div>
+                <div className="headerButtons ml-3">
+                  <MdFavoriteBorder />
+                </div>
+                <div className="headerButtons ml-3">
+                  <AiOutlineUser />
+                </div>
+              </div>
             </div>
-            <div className="flex items-center">
-              <div className="headerButtons">
-                <PiShoppingCartSimple />
-                <div className="text-xs pl-1">Cart</div>
+          </Container>
+        </div>
+        <Container>
+          <div className="header_bottom flex justify-between py-3">
+            <div className="header_bottom-left flex">
+              <div className="categories border flex items-center text-sm  p-2 rounded-md">
+                <RxHamburgerMenu className="mr-2" />
+                <div>All Categories</div>
               </div>
-              <div className="headerButtons ml-3">
-                <MdFavoriteBorder />
-              </div>
-              <div className="headerButtons ml-3">
-                <AiOutlineUser />
-              </div>
+              <nav className="menu flex items-center">
+                <ul className="flex items-center text-sm">
+                  <li className="menuItem">Home</li>
+                  <li className="menuItem">Shop</li>
+                  <li className="menuItem">Product</li>
+                  <li className="menuItem">Pages</li>
+                  <li className="menuItem">About</li>
+                </ul>
+              </nav>
+            </div>
+            <div className="flex header_bottom-right text-sm">
+              <div>Contact:</div>
+              <div>(808) 555-0111</div>
             </div>
           </div>
         </Container>
-      </div>
-      <Container>
-        <div className="header_bottom flex justify-between py-3">
-          <div className="header_bottom-left flex">
-            <div className="categories border flex items-center text-sm mr-5 p-2">
-              <RxHamburgerMenu  className="mr-2"/>
-              <div>All Categories</div>
-            </div>
-            <nav className="menu flex items-center">
-              <ul className="flex items-center">
-                <li>Home</li>
-                <li>Shop</li>
-                <li>Product</li>
-                <li>Pages</li>
-                <li>About</li>
-              </ul>
-            </nav>
-          </div>
-          <div className="flex header_bottom-right text-sm">
-            <div>Contact:</div>
-            <div>(808) 555-0111</div>
-          </div>
-        </div>
-      </Container>
-    </header>
+      </header>
+     
+  
   );
 };
 
