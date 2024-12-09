0493883d2c53516dc6f3f1589bade381b1543b3c
Bahrom <bahram101@mail.ru>
2024-12-09 15:23:37 +0500

    add images to swiper

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,22 +1,36 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination, Navigation, Keyboard } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
+import Image from "next/image";
+import img03 from "@/assets/images/03.jpg";
+import img02 from '@/assets/images/02.jpg'
 
 const Home = () => {
   return (
-    <div className="h-[600px]">
+    <div className="swiper-block h-[700px]">
       <Swiper
         pagination={true}
-        modules={[Pagination, Navigation]}
+        modules={[Pagination, Navigation, Keyboard]}
         className="mySwiper h-full"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide className="">
+          <Image
+            src={img02}
+            className="w-full h-full object-cover"
+            alt="Picture of the author"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <Image
+            src={img03}
+            className="w-full h-full object-cover"
+            alt="Picture of the author"
+          />
+        </SwiperSlide>
       </Swiper>
     </div>
   );
